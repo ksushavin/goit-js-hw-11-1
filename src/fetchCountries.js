6402fe3f9f7035c41fb3713af434db4ad7946bf8
Oksana Bulakh <ksushavin15@gmail.com>
2022-08-06 16:59:54 +0200

    перенесоно всю логіку в основну  функцію

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,17 +1,9 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-export function fetchCountries(name) {
-  
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`)
-        .then(response => {
-            if (response.status === 404) {
-                Notify.failure("Oops, there is no country with that name");
-                return Promise.reject('not found');
-            }
-            return response.json();
-        })
-        .then(countriesArray => {
-            return countriesArray
-        })
-        .catch (error => console.log("error", error))
+const URL = 'https://restcountries.com/';
+
+export default function fetchCountries(name) {
+   
+    return fetch(`${URL}v3.1/name/${name}?fields=name,capital,population,languages,flags`)
+    .then(response => response.json()
+    )
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchCountries } from "./fetchCountries";
+import fetchCountries from "./fetchCountries";
 import './css/styles.css';
 
 const inputRef = document.querySelector("#search-box");
 const countryListFef = document.querySelector(".country-list");
 const countryInfoFef = document.querySelector(".country-info");
 
-
-
 const DEBOUNCE_DELAY = 300;
 inputRef.addEventListener("input", debounce(onSearchBoxInput, DEBOUNCE_DELAY ));
+
 
 function onSearchBoxInput(e) {
     countryListFef.innerHTML = "";
@@ -20,24 +19,32 @@ function onSearchBoxInput(e) {
     if (!query) {
         return
     }
-    fetchCountries(query)
+
+    fetchCountries(query) 
         .then(countriesArray => {
+            console.log(countriesArray);
+            console.log(countriesArray.length);
             if (countriesArray.length > 10) {
                 Notify.info('Too many matches found. Please enter a more specific name.');
                 return
-            } else if (countriesArray.length >= 2 && countriesArray.length <= 11) {
+            } else if (countriesArray.length >1 && countriesArray.length <11) {
                 showCountriesList(countriesArray);
                 return 
             } else if (countriesArray.length === 1) {
                 showCountryCard(countriesArray);
                 return
-            } else if (countriesArray.length === undefined) { 
-                return Promise.reject
+            } else if (countriesArray.length === 0 || countriesArray.length === undefined) {
+                return Promise.reject('code 404'); 
             }
-        }).catch(error => console.log("error", error))
+        }).catch(error => {
+            if (error === "code 404") {
+                Notify.failure("Oops, there is no country with that name");
+            }
+            console.log("error", error);
+         })      
 }
 
-
+// 
 function showCountriesList(countriesArray) {
     countryListFef.insertAdjacentHTML("beforeend", generateCountriesList(countriesArray));
 }
@@ -52,14 +59,17 @@ function generateCountriesList(countriesArray) {
         ${flags.png ? `<img src="${flags.png}"` : `<img src="${flags.svg}"`} 
             alt ="Flag of ${name.official}"
             class = "country-pic"}>
-        <p class="country-name">${name.official}</p></li>`
+        <p class="country-name">${name.official}</p>
+    </li>`
     ).join(""); 
     return сountriesListMarkup;
 }
 
 
 function generateCountryCard(countriesArray) {
-    const сountryCardMarkup = countriesArray.map(({ name, flags, capital, population, languages }) =>
+    const { name, flags, capital, population, languages } = countriesArray[0];
+  
+    const сountryCardMarkup =
         `<div class = "pic-wraper">
             ${flags.png ? `<img src="${flags.png}"` : `<img src="${flags.svg}"`}
                 alt ="Flag of ${name.official}"
@@ -77,8 +87,6 @@ function generateCountryCard(countriesArray) {
             <p class="country-description">
                 Languages:
                 <span class = "span-text">${Object.values(languages)}</span>
-            </p>`
-    ).join("");
+            </p>`;
     return сountryCardMarkup;
 }
-
